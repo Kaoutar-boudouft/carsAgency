d2f66b59b0656a6b2d01c4921670693ab8f5226a
kaoutar boudouft <kaoutarboudouft2@gmail.com>
2023-02-13 12:03:09 +0100

    setting up the directories : controllers/models/services/repositories and templates

---
+++ src/main/java/com.carsAgency/Controllers/ApplicationController.java
@@ -0,0 +1,13 @@
+package com.carsAgency.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
