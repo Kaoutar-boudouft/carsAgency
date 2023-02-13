89756ab8f9edf239c759db6fe106f48aab791701
kaoutar boudouft <kaoutarboudouft2@gmail.com>
2023-02-13 09:40:05 +0100

    setting up the project structure and dependencies

---
+++ src/main/java/com.carsAgency/CarsAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.carsagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsAgencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carsagency/CarsAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsAgencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
